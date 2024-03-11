936c776a8b9da60c2ea55376b54d6196f45d9676
Gabrielluiz998 <gllf1@aluno.ifal.edu.br>
2024-03-11 09:14:31 -0300

    add file

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App!</Text>      
     </View>
+    
   );
 }
 
@@ -16,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  
+  subtitlo: {
+
   },
 });
