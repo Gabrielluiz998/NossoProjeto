66d4b8bee0e7b04072fdab192c52f4c7e382c6f9
MelrickDavison <mdfr1@aluno.ifal.edu.br>
2024-03-25 09:26:07 -0300

    método de stado e contador de clique adicionado

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Gato from './Gato';
+import Cliques  from './Cliques';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Bem-vindo!</Text>      
+      <Text>Bem-vindo!</Text>
+      <Gato idade={1} /> 
+      <Cliques/>
     </View>
     
   );
@@ -17,9 +21,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  
-  subtitlo: {
-
   },
 });
